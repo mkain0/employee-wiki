30884b1f2a94081ed7c9df61ad858d397a43e27e
magakain <kain.magali@gmail.com>
2015-12-29 08:52:39 -0300

    Add flash messages

--- Employee Wiki/routes/main.js
+++ Employee Wiki/routes/main.js
@@ -1,54 +1,56 @@
 var app = module.parent.exports.app;
 var Employees = require('../models/employees.js')
 
-app.get('/list', function(req, res){
+app.get('/panel/employees', function(req, res){
     var msg = req.flash('message'); // Read the flash message
     Employees.find({}, function(err, docs){
         // res.render('list', { title: 'List', employees: docs});
-        res.render('list', { title: 'List', persons: docs, flashmsg: msg}); // Pass Flash Message to the view
+        res.render('list', { title: 'Employee List', employees: docs, flashmsg: msg}); // Pass Flash Message to the view
     });
 });
 
-app.get('/employees/new', function(req, res){
+app.get('/panel/employees/new', function(req, res){
     req.flash('message', 'You have successfully created your Employee'); // Save the flash message
-    res.render('new', { title: 'New'});
+    res.render('new', { title: 'New Employee'});
 });
 
-app.post('/employees/new', function(req, res){
-    console.log(req.body);
+app.post('/panel/employees/new', function(req, res){
+   console.log(req.body);
    var e = new Employees({ firtsName: req.body.firtsName, 
-   	lastName: req.body.lastName, email: req.body.email, 
-   	password: req.body.password });
+   lastName: req.body.lastName, email: req.body.email, 
+   password: req.body.password });
    e.save(function(err, doc){
           if(!err){
-            res.redirect('/list');
+            res.redirect('/panel/employees');
        } else {
            res.end(err);    
        }    
    });
  });
 
-app.get('/employees/delete/:id', function(req, res){
+app.get('/panel/employees/delete/:id', function(req, res){
+    req.flash('message', 'You have successfully deleted your Employee'); // Save the flash message
     Employees.remove({ _id: req.params.id }, function(err, doc){
         if(!err){
-            res.redirect('/list');
+            res.redirect('/panel/employees');
         } else {
             res.end(err);    
         }    
     });
 });
 
-app.get('/employees/edit/:id', function(req, res){
+app.get('/panel/employees/edit/:id', function(req, res){
+    req.flash('message', 'You have successfully edited your Employee'); // Save the flash message
     Employees.findOne({ _id: req.params.id }, function(err, doc){
         if(!err){
-            res.render('edit', { title: 'Edit', employee: doc});
+            res.render('edit', { title: 'Edit Employee', employee: doc});
         } else {
             res.end(err);    
         }    
     });
 });
 
-app.post('/employees/edit/:id', function(req, res){
+app.post('/panel/employees/edit/:id', function(req, res){
     Employees.findOne({ _id: req.params.id }, function(err, doc){
         if(!err){
             doc.firtsName = req.body.firtsName; 
@@ -56,7 +58,7 @@ app.post('/employees/edit/:id', function(req, res){
             doc.email = req.body.email;
             doc.save(function(err, doc){
                 if(!err){
-                    res.redirect('/list');
+                    res.redirect('/panel/employees');
                 } else {
                     res.end(err);    
                 }    
